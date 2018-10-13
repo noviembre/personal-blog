a17ae0e50f9cc42080ae156161bf99046c3c5309
Artemis Deepin 8GB <perudragon@hotmail.com>
2018-10-13 09:37:45 -0400

    03 Master Layout - Frontend - Setting up
    se hizo con algunos archivos.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PublicController@index' );
+Route::get('post/{id}', 'PublicController@singlePost' );
+Route::get('about', 'PublicController@about' );
+Route::get('contact', 'PublicController@contact' );
+Route::post('contact', 'PublicController@contactPost' );
