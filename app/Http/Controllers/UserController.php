0976bbea2d0b34bfe68d8ab637eb416a72c47173
Artemis Deepin 8GB <perudragon@hotmail.com>
2018-10-16 08:51:54 -0400

    12.1 Admin User Profile - Edit
    Edita los datos del usuario que se ha logeado.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function profile()
+    {
+        return view('user.profile');
+    }
+
+    public function profilePost(Request $request)
+    {
+        $user = Auth::user();
+        $user->name = $request['name'];
+        $user->email = $request['email'];
+        $user->save();
+
+        return back();
+    }
 }
